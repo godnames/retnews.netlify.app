8430487eb223f2db5b74d6e6e284dcdf151054dc
godnames <niranjan.hospitalityminds@gmail.com>
2024-01-11 13:06:48 +0530

    first

--- js/date.js
+++ js/date.js
@@ -3,3 +3,7 @@ var datetime = now.toDateString();
 
 // Insert date and time into HTML
 document.getElementById("demo").innerHTML = datetime;
+
+var today = new Date()
+var year = today.getFullYear()
+document.getElementById("years").innerHTML = year;
